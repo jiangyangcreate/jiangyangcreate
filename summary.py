e11e960a61f505a808f47230a421aa40aca64219
jiangyangcreate <jiangyangcreate@gmail.com>
2024-02-02 10:12:32 +0800

    🐛 修复分割报错

--- summary.py
+++ summary.py
@@ -50,7 +50,7 @@ class BaiduAI:
 
 def get_summary(content: str) -> str:
     # 剪切掉摘要部分，仅保留正文
-    selector = Selector(text=content.split('此内容根据文章生成，仅用于文章内容的解释与总结</div></div></div><p>')[1])
+    selector = Selector(text=content.split('此内容根据文章生成，仅用于文章内容的解释与总结')[1])
     content_format = "".join(selector.xpath(".//text()").getall())
     ai = BaiduAI()
     summary = ai.get_result(content_format)
